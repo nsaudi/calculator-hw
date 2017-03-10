854f24722fc0f5740391186841f079c9da51856e
Dr Moe <snannann@Moes-MacBook-Air.local>
2017-03-10 14:01:22 -0500

    new branch

--- calc.js
+++ calc.js
@@ -18,6 +18,8 @@ $(document).ready(function () {
 		$(".x").text(x);
 		$(".y").text(y);
 
+		$('#results').addClass(".show");
+
 	});
 
 });
@@ -36,4 +38,5 @@ function multiply(a, b) {
 
 function divide(a, b) {
 	return a / b;
+}
 }
