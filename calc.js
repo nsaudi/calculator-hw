26a49e73cc25def3d823e9db900e0cee8446c44c
nsaudi <snannann@Moes-MacBook-Air.local>
2017-03-09 20:48:26 -0500

    Adding the code

---
+++ calc.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+	$("#button").on("click", function (e) {
+		e.preventDefault();
+
+		var x = parseFloat($("#x").val());
+		var y = parseFloat($("#y").val());
+
+		var add = sum(x, y);
+		var sub = subtract(x, y);
+		var mult = multiply(x, y);
+		var div = divide(x, y);
+
+
+		$("#zAdd").text(add);
+		$("#zSub").text(sub);
+		$("#zMult").text(mult);
+		$("#zDiv").text(div);
+		$(".x").text(x);
+		$(".y").text(y);
+
+	});
+
+});
+
+function sum(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return b - a;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
